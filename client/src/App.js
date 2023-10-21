62d7cea17549b106b382b2368fd9124301a19558
Egor <e.moisei1@gmail.com>
2023-10-22 01:45:11 +0300

    init commit

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App">happy</div>;
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+
+const app = express();
+const PORT = config.get("serverPort");
+
+const start = async () => {
+  try {
+    await mongoose.connect(config.get("mongoURL"));
+    app.listen(PORT, () => {
+      console.log(`server started on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+start();
