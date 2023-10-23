eb279a177618e2d6c83931cafd61a93725aa8a91
Egor <e.moisei1@gmail.com>
2023-10-23 14:26:32 +0300

    create user and files reducers

---
+++ client/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="App">happy!!!</div>;
+}
+
+export default App;
---
+++ client/src/reducers/index.js
@@ -0,0 +1,16 @@
+import { combineReducers, applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import userReducer from "./userReducer";
+import fileReducer from "./fileReducer";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  files: fileReducer,
+});
+
+export const store = configureStore(
+  { reducer: rootReducer },
+  composeWithDevTools(applyMiddleware(thunk))
+);
