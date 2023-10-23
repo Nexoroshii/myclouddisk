2a26f1edbdda5129dbddd7717b8dc8042eafbc7d
Egor <e.moisei1@gmail.com>
2023-10-23 14:55:22 +0300

    create navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
+import Navbar from "./components/navbar/Navbar";
+
 function App() {
-  return <div className="App">happy!!!</div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
