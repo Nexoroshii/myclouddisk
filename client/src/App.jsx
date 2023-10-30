ef36f4fde17f41ae2db0cf0922edcbaea1563f45
Egor <e.moisei1@gmail.com>
2023-10-30 14:26:33 +0300

    finish with token

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,28 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Registration from "./components/register/Registration";
 import Login from "./components/login/Login";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { auth } from "./actions/user";
 
 function App() {
+  const isAuth = useSelector((state) => state.user.isAuth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(auth());
+  }, []);
+
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/registration" element={<Registration />} />
-        </Routes>
+        {!isAuth && (
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/registration" element={<Registration />} />
+          </Routes>
+        )}
       </div>
     </BrowserRouter>
   );
